197c9d0a9f036b6ba412ce0fe544879df16b6b41
bhuphathi <bhuphathi@gmail.com>
2023-08-04 18:06:23 +0530

    feat: meal detail screen with listview

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -15,7 +15,51 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl),
+      body: ListView(
+        children: [
+          Image.network(meal.imageUrl),
+          const SizedBox(height: 16),
+          Text(
+            "Ingredients",
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 16),
+          for (final ingredient in meal.ingredients)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          const SizedBox(height: 16),
+          Text(
+            "Steps",
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 16),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                step,
+                textAlign: TextAlign.justify,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
