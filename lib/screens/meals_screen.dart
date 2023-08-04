d4ff2ac615734366bb90d60dab7de95b5e0164f6
bhuphathi <bhuphathi@gmail.com>
2023-08-04 15:34:59 +0530

    feat: pageroute added to basic meal detail screen

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal_model.dart';
+import 'package:meals_app/screens/meal_details_screen.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -7,6 +8,14 @@ class MealsScreen extends StatelessWidget {
 
   final String title;
   final List<MealModel> meals;
+
+  void onSelectMeal(BuildContext context, MealModel meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +45,11 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+        itemBuilder: (ctx, index) => MealItem(
+            meal: meals[index],
+            onSelectMeal: (meal) {
+              onSelectMeal(context, meal);
+            }),
       );
     }
 
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,9 +4,14 @@ import 'package:meals_app/widgets/meal_item_detail.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({
+    super.key,
+    required this.meal,
+    required this.onSelectMeal,
+  });
 
   final MealModel meal;
+  final Function(MealModel meal) onSelectMeal;
 
   String get complexityText {
     final complexityName = meal.complexity.name;
@@ -26,7 +31,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
