9f269a8bd3604f572c40d096a6fd8786f63cbd15
bhuphathi <bhuphathi@gmail.com>
2023-08-05 09:35:34 +0530

    feat: scaffold added to catefory screen

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -23,23 +23,28 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 12,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick Categories"),
       ),
-      padding: const EdgeInsets.all(12),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _onSelectCategory(context, category);
-            },
-          )
-      ],
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+        ),
+        padding: const EdgeInsets.all(12),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _onSelectCategory(context, category);
+              },
+            )
+        ],
+      ),
     );
   }
 }
