a19e830eacaeaab6a6a5420c3d1c4a692b1e5b1d
bhuphathi <bhuphathi@gmail.com>
2023-08-13 17:29:43 +0530

    fix: swapping favorite btn  based on state

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -13,24 +13,28 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
-              onPressed: () {
-                final wasAdded = ref
-                    .read(favoriteMealsProvider.notifier)
-                    .toggleFavoriteMeals(meal);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(wasAdded
-                        ? "Added ${meal.title} to favorites"
-                        : "Removed ${meal.title} from favorites"),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.star))
+            onPressed: () {
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleFavoriteMeals(meal);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(wasAdded
+                      ? "Added ${meal.title} to favorites"
+                      : "Removed ${meal.title} from favorites"),
+                ),
+              );
+            },
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border_outlined),
+          ),
         ],
       ),
       body: ListView(
